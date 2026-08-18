f4921b1e613727f9d3d1a6d5a6f56dcc3d2adf13
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-18 10:23:48 +0700

    implementai fungsi penulisan dan pembacaan file dengan input dari pengguna.

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -24,4 +24,6 @@ int main()
     {
         cout << "- ";
         //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi jika baris hanya berisi huruf q
     }
