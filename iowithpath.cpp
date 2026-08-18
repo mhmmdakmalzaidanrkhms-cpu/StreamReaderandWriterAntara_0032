d6e3c99b58d21c57b7c10cc48d0c53f4457c3b02
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-18 10:28:12 +0700

    Implementasikan fungsi membaca file dan menampilkan isinya

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -39,4 +39,14 @@ int main()
 
     //menunjuk ke sebuah file dengan ekstensi .txt
     infile.open(NamaFile + ".txt", ios::in);
-}
+
+    cout << endl << "=> Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk menampilkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+    }
