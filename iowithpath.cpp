632bb325d576a7b4709bba9665c9ef3eb2cb8e20
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-08-18 10:19:09 +0700

    **“Implement file writing and reading functionality with user input”**
    Artinya dalam bahasa Indonesia:
    
    **“Implementasikan fungsi penulisan dan pembacaan file dengan input dari pengguna.”**

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -11,4 +11,9 @@ int main()
     //membuat input untuk menangkap nama file
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open("C:\\Users\\LENOVO\\Documents\\contoh\\" + NamaFile + ".txt", ios::out);
 }
